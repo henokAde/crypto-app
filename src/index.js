38b337888ca9bd89e6015a182291e4f37e1608d6
henokADE <adeyamah@yahoo.fr>
2022-01-02 16:05:36 +0100

    adding redux toolkit and fetching global data in homePage

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
+import { Provider } from 'react-redux';
 
 import App from './App';
+import store from './app/store'
 ReactDOM.render(
     <React.StrictMode>
       <Router>
-        
+        <Provider store= {store}>
           <App />
-       
+        </Provider>
       </Router>
     </React.StrictMode>,
     document.getElementById('root'),
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,35 @@
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': '6991a917f4msha604b7540dec1e7p18706djsn28808e900d5f'
+};
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) =>({url, headers: cryptoApiHeaders})
+export const cryptoApi = createApi({
+    reducerPath:'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) =>({
+        getCryptos: builder.query({
+            query:() =>createRequest('/coins')
+        })
+    })
+});
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi;
+
+// var options = {
+//     method: 'GET',
+//     url: 'https://coinranking1.p.rapidapi.com/exchanges',
+//     params: {
+//       referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//       uuids: '-zdvbieRdZ',
+//       limit: '50',
+//       offset: '0',
+//       orderBy: '24hVolume',
+//       orderDirection: 'desc'
+//     }
+// }
