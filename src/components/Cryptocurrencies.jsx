f9339e0ba895711fc544b21b179aa7686a0625e6
henokADE <adeyamah@yahoo.fr>
2022-01-04 09:06:13 +0100

    adding input search filter on cryptocurrency page

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -1,23 +1,40 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import React from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import {Card, Row, Col, Input} from 'antd';
 import {useGetCryptosQuery} from '../services/cryptoApi';
+import './cryptocurrencies.css'
 
 const Cryptocurrencies = ({simplified}) => {
     const count = simplified ? 10 : 100;
     const {data:cryptosList, isFetching } = useGetCryptosQuery(count);
-    const [cryptos, setCrypto] = useState(cryptosList?.data?.coins)
+    const [cryptos, setCryptos] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
     // console.log(cryptos);
+    useEffect(() => {
+        // setCryptos(cryptosList?.data?.coins);
+        const filteredData = cryptosList?.data?.coins.filter((coin) => coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+        setCryptos(filteredData)
+    }, [cryptosList, searchTerm])
+    if (isFetching) return 'Loading ...'
+
     return (
         <>
+        {!simplified &&(
+
+        <div className="search_crypto">
+            <Input className="searchInput"
+            placeholder="Search Cryptocurrency"
+            onChange={(e) => setSearchTerm(e.target.value)} />
+        </div>
+        )}
             <Row gutter = {[32, 32]} className="crypto-card-container">
                 {
-                    cryptos.map((currency) =>(
+                    cryptos?.map((currency) =>(
                         <Col xs={24} sm={12} lg={6} className="crypto-card" key = {currency.id}>
                             <Link to = {`/crypto/${currency.id}`}>
-                                <Card
+                                <Card className="card"
                                     title = {`${currency.rank}. ${currency.name}`}
                                     extra = {<img className= "crypto-image" src={currency.iconUrl}/>}
                                     hoverable
